9d8ab5856d08c3d4805aa611e1ec1808af896500
vegetablestoptalking <magerram13092000@gmail.com>
2020-09-08 00:18:33 +0300

    some inserts

--- src/main/java/crutchesbicycles/studyhelper/controller/StudentMaterialController.java
+++ src/main/java/crutchesbicycles/studyhelper/controller/StudentMaterialController.java
@@ -1,9 +1,12 @@
 package crutchesbicycles.studyhelper.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/students")
 public class StudentMaterialController {
-    
+    //todo: не забыть передавать в FileUploadController уже модифицированное имя файла, чтобы не было колизий
+
 }
