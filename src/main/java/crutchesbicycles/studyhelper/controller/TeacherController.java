12fe45018dd600ef3442f412cb60a8aa83935578
vgtstptlk <magerram13092000@gmail.com>
2020-09-17 00:56:04 +0300

    test

--- src/main/java/crutchesbicycles/studyhelper/controller/TeacherController.java
+++ src/main/java/crutchesbicycles/studyhelper/controller/TeacherController.java
@@ -3,6 +3,7 @@ package crutchesbicycles.studyhelper.controller;
 import crutchesbicycles.studyhelper.domain.Teachers;
 import crutchesbicycles.studyhelper.repos.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +24,8 @@ public class TeacherController {
     ResponseEntity<?> createTeacher(@RequestParam String firstName, @RequestParam String secondName,
                                     @RequestParam String patronymic, @RequestParam String email){
         Teachers  tempTeacher = new Teachers(firstName, secondName, patronymic, email);
+        teacherRepository.save(tempTeacher);
+        return new ResponseEntity<>("Teacher was created", HttpStatus.CREATED);
 
     }
 
