773f5ea504570e9c71a954a182d5fb76c603fc65
vgtstptlk <magerram13092000@gmail.com>
2020-10-24 04:02:48 +0300

    fix

--- src/main/java/crutchesbicycles/studyhelper/config/SecurityConfig.java
+++ src/main/java/crutchesbicycles/studyhelper/config/SecurityConfig.java
@@ -1,35 +1,35 @@
-package com.apress.SpringSecurityX509Auth.Configuration;
-
-import crutchesbicycles.studyhelper.domain.Account;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/**").permitAll()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .csrf().disable();
-    }
-
-}
+//package com.apress.SpringSecurityX509Auth.Configuration;
+//
+//import crutchesbicycles.studyhelper.domain.Account;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+////@Configuration
+////@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+//////        http
+//////                .authorizeRequests()
+//////                .antMatchers("/**").permitAll()
+//////                .and()
+//////                .httpBasic()
+//////                .and()
+//////                .csrf().disable();
+////    }
+//
+//}
